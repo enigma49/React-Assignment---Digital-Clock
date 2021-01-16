72e4b1dad2c1da4f4b5c698364684bf036542d5b
Hamza Shaikh <shaikhhamza157@gmail.com>
2021-01-16 17:45:34 +0530

    aquib

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,4 @@
-// import React, { Component, useState } from "react";
-// import "../styles/App.css";
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       time: new Date(),
-//     };
-//     this.intervalId = null;
-//   }
-//   getTimeString() {
-// //     const currTime = this.state.time;
-// //     const [hours, mins, secs] = [
-// //       currTime.getHours(),
-// //       currTime.getMinutes(),
-// //       currTime.getSeconds(),
-// //     ];
-
-// //     const amOrPm = hours >= 12 ? "PM" : "AM";
-// //     const twleveHourFormat = hours > 12 ? hours - 12 : hours;
-// //     // const hourString = this.padNumberToTwoDigits(twleveHourFormat);
-// //     const hourString = twleveHourFormat;
-// //     const minuteString = this.padNumberToTwoDigits(mins);
-// //     const secsString = this.padNumberToTwoDigits(secs);
-// //     const timeString = `${hourString}:${minuteString}:${secsString} ${amOrPm}`;
-//     const timeString = new Date().toLocaleString();
-//     return timeString;
-//   }
-
-// //   padNumberToTwoDigits(num) {
-// //     return num >= 10 ? num : "0" + num.toString();
-// //   }
-
-//   componentDidMount() {
-//       this.intervalId = setInterval(() => {
-//           this.setState({
-//               time: new Date()
-//           })
-//       },1*1000)
-//   }
-
-//   componentWillUnMount(){
-//       clearInterval(this.intervalId);
-//   }
-
-//   render() {
-//     return (
-//       <div className="Clock">
-//         <h3 id="time">{this.getTimeString()}</h3>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
+  
 import React, { Component, useState } from "react";
 import "../styles/App.css";
 
@@ -76,40 +11,29 @@ class App extends Component {
     this.intervalId = null;
   }
   getTimeString() {
-//     const currTime = this.state.time;
-//     const [hours, mins, secs] = [
-//       currTime.getHours(),
-//       currTime.getMinutes(),
-//       currTime.getSeconds(),
+    const currentTime = this.state.time;
+//     const pad = (x) => x.toString().padStart(2, "0");
+//     const [hrs, min, sec] = [
+//       currentTime.getHours(),
+//       currentTime.getMinutes(),
+//       currentTime.getSeconds(),
 //     ];
-
-//     const amOrPm = hours >= 12 ? "PM" : "AM";
-//     const twleveHourFormat = hours > 12 ? hours - 12 : hours;
-//     // const hourString = this.padNumberToTwoDigits(twleveHourFormat);
-//     const hourString = twleveHourFormat;
-//     const minuteString = this.padNumberToTwoDigits(mins);
-//     const secsString = this.padNumberToTwoDigits(secs);
-//     const timeString = `${hourString}:${minuteString}:${secsString} ${amOrPm}`;
-    const timeString = new Date().toLocaleTimeString('en-US',{ hour12: true });
-    return timeString;
+//     const pm = hrs >= 12 ? " PM" : " AM";
+//     const hr = hrs > 12 ? hrs - 12 : hrs;
+    let string = new Date().toLocaleTimeString();
+//     `${pad(hr)}:${pad(min)}:${pad(sec)}${pm}`;
+    return string;
   }
-
-//   padNumberToTwoDigits(num) {
-//     return num >= 10 ? num : "0" + num.toString();
-//   }
-
   componentDidMount() {
-      this.intervalId = setInterval(() => {
-          this.setState({
-              time: new Date()
-          })
-      },1*1000)
+    this.intervalId = setInterval(() => {
+      this.setState({
+        time: new Date(),
+      });
+    }, 1 * 1000);
   }
-
-  componentWillUnMount(){
-      clearInterval(this.intervalId);
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
   }
-
   render() {
     return (
       <div className="Clock">
